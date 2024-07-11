3a480a4bd70e11eec4e2864203c9ad43dde6e3fb
Ibrangrd <ibrangrd@gmail.com>
2024-07-11 21:12:33 +0530

    add some function

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-let design = document.getElementById("#back");
-let orientation = document.getElementById("#or");
-
+let design = document.getElementById("back");
+let col = document.getElementById("name")
+let current_design = document.getElementsByClassName("box")[0];
+ 
+design.addEventListener("change" , () => {
+  if(design.value === "1"){
+   current_design.style.backgroundImage = "url('3d1.jpg')";
+  }
+  else if (design.value === "2"){
+    current_design.style.backgroundImage = "url('3d2.jpg')";
+  }
+  else if (design.value === "3"){
+    current_design.style.backgroundImage = "url('3d3.jpg')";
+    col.style.color = "red";
+  }
+  else if (design.value === "4"){
+    current_design.style.backgroundImage = "url('3d4.jpg')";
+  }
+  else if (design.value === "5"){
+    current_design.style.backgroundImage = "url('3d5.jpg')";
+  }
+  else if (design.value === "6"){
+    current_design.style.backgroundImage = "url('3d6.jpg')";
+  }
+  else if (design.value === "7"){
+    current_design.style.backgroundImage = "url('3d7.jpg')";
+  }
+  else
+  {
+    current_design.style.backgroundImage = "url('3d8.jpg')";
+  }
+})
